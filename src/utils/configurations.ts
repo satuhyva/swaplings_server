690148902d467785626cbc8e334bf5558b9c5ffb
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2021-06-28 10:04:23 +0300

    github actions mongodb trial 4

--- src/utils/configurations.ts
+++ src/utils/configurations.ts
@@ -11,11 +11,16 @@ const JWT_SECRET = process.env.JWT_SECRET || ''
 const E2E = process.env.E2E || ''
 const DEV = process.env.DEV || ''
 
-
-let MONGO_DB_URL = process.env.MONGO_DB_URL
-if (process.env.NODE_ENV === 'test' || process.env.E2E === 'e2e' || process.env.DEV === 'dev') {
+let MONGO_DB_URL: string | undefined = ''
+if (process.env.NODE_ENV === 'githubaction') {
+    MONGO_DB_URL = 'mongodb://localhost:27017'
+} else if (process.env.NODE_ENV === 'test' || process.env.E2E === 'e2e' || process.env.DEV === 'dev') {
     MONGO_DB_URL = process.env.MONGO_DB_TEST_URL
+} else {
+    MONGO_DB_URL = process.env.MONGO_DB_URL
 }
+
+
 if (!MONGO_DB_URL) throw new Error('MONGO_DB_URL is missing a value!')
 
 
